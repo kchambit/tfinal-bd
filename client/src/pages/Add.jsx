2e4245f5a4cea7915c2860cc088718a7842237f9
Kevin Chambi <kchambit@unsa.edu.pe>
2022-12-18 18:16:52 -0500

    first commit

---
+++ client/src/pages/Add.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+const Add = () => {
+  const [book, setBook] = React.useState({
+    title: '',
+    desc: '',
+    price: null,
+    cover: ''
+  })
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setBook({...book, [e.target.name]: e.target.value})
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault()
+    try {
+      await axios.post('http://localhost:8800/books', book)
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className='form'>
+        <h1>Add new book</h1>
+        <input type="text" placeholder='title' name='title' onChange={handleChange}/>
+        <input type="text" placeholder='desc' name='desc' onChange={handleChange}/>
+        <input type="number" placeholder='price' name='price' onChange={handleChange}/>
+        <input type="text" placeholder='cover' name='cover' onChange={handleChange}/>
+        <button onClick={handleClick}>Add</button>
+    </div>
+  )
+}
+
+export default Add
